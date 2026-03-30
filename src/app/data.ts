dcb3441ad3913626de28f18e4fb8ae7f9eec3e43
Aminata YADE <aminata.yade@isen.yncrea.fr>
2026-03-30 10:39:44 +0200

    add receipes listes

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,8 +1,37 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Data {
+  private httpClient = inject(HttpClient);
+  private apiKey = '8c4b46ef1742496783a6f7dde1fb54dc';
   
+  // Tableau pour stocker vos recettes
+  public recipes: Array<any> = [];
+
+  constructor() {
+    // Appel vers Spoonacular (Exemple : 10 recettes complexes)
+    this.httpClient
+      .get<any>(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${this.apiKey}&number=10&addRecipeInformation=true`)
+      .subscribe({
+        next: (value: any) => {
+          // Spoonacular renvoie les résultats dans un tableau nommé 'results'
+          const newArray = value.results.map((el: any) => {
+            return {
+              id: el.id,
+              name: el.title,
+              description: el.summary?.replace(/<[^>]*>/g, '').slice(0, 150) + '...', // Nettoyage du HTML
+              img: el.image,
+              time: el.readyInMinutes // Exemple de donnée supplémentaire
+            };
+          });
+          
+          this.recipes = newArray;
+          console.log('Recettes chargées :', this.recipes);
+        },
+        error: (err) => console.error('Erreur API :', err)
+      });
+  }
 }
--- src/app/recipe-list/recipe-list.ts
+++ src/app/recipe-list/recipe-list.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Data } from '../data';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './recipe-list.html',
   styleUrl: './recipe-list.css',
 })
-export class RecipeList {}
+export class RecipeList {
+  public dataService = inject(Data);
+}
+
+
+
