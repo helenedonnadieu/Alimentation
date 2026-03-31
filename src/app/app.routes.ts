7208e8f5a7f2d751538e23bafeb5ff4122f50d61
Aminata YADE <aminata.yade@isen.yncrea.fr>
2026-03-31 10:19:30 +0200

    ajout de la page about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Body } from './body/body';
 import { RecipeList } from './recipe-list/recipe-list';
 import { RecipeCard } from './recipe-card/recipe-card';
 import { RecipeDetails } from './recipe-details/recipe-details';
+import { About } from './about/about';
 export const routes: Routes = [
   { 
     path: '', 
@@ -12,6 +13,7 @@ export const routes: Routes = [
       { path: '', component: RecipeList },
       { path: 'recipes', component: RecipeList },
       { path: 'favorites', component: RecipeList, data: { favoritesOnly: true } },
+      { path: 'about', component: About },
 
       { path: 'recipes/:id', component: RecipeCard }, 
       { path: 'details/:id', component: RecipeDetails },
