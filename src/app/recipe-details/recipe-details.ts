0be814fbb6b09aae67bb2ffdc9589ce2fbc52f34
Aminata YADE <aminata.yade@isen.yncrea.fr>
2026-04-07 12:58:00 +0200

    modification des couleurs

--- src/app/recipe-details/recipe-details.ts
+++ src/app/recipe-details/recipe-details.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, computed, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
-import { Data } from '../data';
 @Component({
   selector: 'app-recipe-details',
   imports: [CommonModule],
@@ -12,49 +12,93 @@ import { Data } from '../data';
 })
 
 export class RecipeDetails implements OnInit {
-  recipe: any;
-  ingredients: any[] = [];
-  steps: any[] = [];
-  //private apiKey = '6891b90d059f49b8b18c3d3800b9cba7';
-   // private apiKey = '8c4b46ef1742496783a6f7dde1fb54dc';
+  private route = inject(ActivatedRoute);
+  private http = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
 
-  //private apiKey ='8c4b46ef1742496783a6f7dde1fb54dc';
+  recipe = signal<any | null>(null);
+  ingredients = signal<any[]>([]);
+  steps = signal<any[]>([]);
+  currentStepIndex = signal(0);
+  stepCount = computed(() => this.steps().length);
+  currentStep = computed(() => this.steps()[this.currentStepIndex()] ?? null);
+  progress = computed(() => {
+    const total = this.stepCount();
+    if (!total) {
+      return 0;
+    }
+
+    return ((this.currentStepIndex() + 1) / total) * 100;
+  });
+  canGoPrevious = computed(() => this.currentStepIndex() > 0);
+  canGoNext = computed(() => this.currentStepIndex() < this.stepCount() - 1);
+
   private apiKey = '0f0185dbc36b48638ef048d3cb6fba64';
 
-
-  constructor(private route: ActivatedRoute, private http: HttpClient) {}
-
   ngOnInit() {
-    const id = this.route.snapshot.paramMap.get('id');
-    this.http.get<any>(
-      `https://api.spoonacular.com/recipes/${id}/information?apiKey=${this.apiKey}&includeNutrition=false`
-    ).subscribe({
-      next: (data) => {
-        this.recipe = {
-          id: data.id,
-          name: data.title,
-          img: data.image,
-          time: data.readyInMinutes,
-          prepTime: data.preparationMinutes,
-          cookTime: data.cookingMinutes,
-          servings: data.servings,
-          vegetarian: data.vegetarian,
-          vegan: data.vegan,
-          glutenFree: data.glutenFree,
-          dairyFree: data.dairyFree,
-          veryHealthy: data.veryHealthy,
-          veryPopular: data.veryPopular,
-          healthScore: data.healthScore,
-          price: (data.pricePerServing / 100).toFixed(2),
-          likes: data.aggregateLikes,
-          dishTypes: data.dishTypes,
-          occasions: data.occasions,
-          sourceUrl: data.sourceUrl,
-        };
-        this.ingredients = data.extendedIngredients || [];
-        this.steps = data.analyzedInstructions?.[0]?.steps || [];
+    this.route.paramMap
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((params) => {
+        const id = params.get('id');
+
+        if (!id) {
+          this.recipe.set(null);
+          this.ingredients.set([]);
+          this.steps.set([]);
+          this.currentStepIndex.set(0);
+          return;
+        }
+
+        this.http.get<any>(
+          `https://api.spoonacular.com/recipes/${id}/information?apiKey=${this.apiKey}&includeNutrition=false`
+        ).subscribe({
+          next: (data) => {
+            this.recipe.set({
+              id: data.id,
+              name: data.title,
+              img: data.image,
+              time: data.readyInMinutes,
+              prepTime: data.preparationMinutes,
+              cookTime: data.cookingMinutes,
+              servings: data.servings,
+              vegetarian: data.vegetarian,
+              vegan: data.vegan,
+              glutenFree: data.glutenFree,
+              dairyFree: data.dairyFree,
+              veryHealthy: data.veryHealthy,
+              veryPopular: data.veryPopular,
+              healthScore: data.healthScore,
+              price: (data.pricePerServing / 100).toFixed(2),
+              likes: data.aggregateLikes,
+              dishTypes: data.dishTypes,
+              occasions: data.occasions,
+              sourceUrl: data.sourceUrl,
+            });
+            this.ingredients.set(data.extendedIngredients || []);
+            this.steps.set(data.analyzedInstructions?.[0]?.steps || []);
+            this.currentStepIndex.set(0);
+          },
+          error: (err) => console.error('Erreur API :', err)
+        });
       },
-      error: (err) => console.error('Erreur API :', err)
-    });
+      );
+  }
+
+  previousStep(): void {
+    if (this.canGoPrevious()) {
+      this.currentStepIndex.update((index) => index - 1);
+    }
+  }
+
+  nextStep(): void {
+    if (this.canGoNext()) {
+      this.currentStepIndex.update((index) => index + 1);
+    }
+  }
+
+  selectStep(stepIndex: number): void {
+    if (stepIndex >= 0 && stepIndex < this.stepCount()) {
+      this.currentStepIndex.set(stepIndex);
+    }
   }
 }
